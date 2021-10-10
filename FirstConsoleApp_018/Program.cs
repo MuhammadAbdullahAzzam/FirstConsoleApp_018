92d815014e77c3d6c52bfcb5db9fb1b013def34f
Muhammad Abdullah Azzam <79858774+MuhammadAbdullahAzzam@users.noreply.github.com>
2021-10-10 21:04:42 +0700

    Add project files.

---
+++ FirstConsoleApp_018/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FirstConsoleApp_018
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Hello World");
+
+            Console.ReadKey();
+        }
+    }
+}
